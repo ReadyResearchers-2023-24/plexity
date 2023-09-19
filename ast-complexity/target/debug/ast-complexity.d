83b641a406801589fa48bd0d14db459e14e5b6ef
Jeff Normile <normile01@allegheny.edu>
2023-09-19 10:08:41 -0400

    Add gitignore; remove compiled artifact

--- ast-complexity/target/debug/ast-complexity.d
+++
@@ -1,1 +0,0 @@
-C:\Users\normi\SE_600&610\jeff-normile-artifact\ast-complexity\target\debug\ast-complexity.exe: C:\Users\normi\SE_600&610\jeff-normile-artifact\ast-complexity\src\main.rs
--- ast-complexity/target/debug/deps/ast_complexity.d
+++
@@ -1,5 +0,0 @@
-C:\Users\normi\SE_600&610\jeff-normile-artifact\ast-complexity\target\debug\deps\ast_complexity.exe: src\main.rs
-
-C:\Users\normi\SE_600&610\jeff-normile-artifact\ast-complexity\target\debug\deps\ast_complexity.d: src\main.rs
-
-src\main.rs:
