51af23cc1a7e583b8b7187c5c6f579ef008c17fe
Jeff Normile <normile01@allegheny.edu>
2023-09-26 15:21:56 -0400

    Fill in missing type annotations for clarity

--- ast-complexity/src/main.rs
+++ ast-complexity/src/main.rs
@@ -4,19 +4,19 @@ use std::fs;
 
 fn read_input_file() -> String {
     let arguments: Vec<String> = env::args().collect();
-    let input_filepath = &arguments[1];
+    let input_filepath: &String = &arguments[1];
     //let selected_language = &arguments[2];
 
     println!();
     println!("Selected input file: {}", input_filepath);
 
-    let file_contents = fs::read_to_string(input_filepath)
+    let file_contents: String = fs::read_to_string(input_filepath)
         .expect("Should have been able to read the file");
 
-    file_contents
+    return file_contents;
 }
 
-fn build_tree(input_file_contents: String) {
+fn build_tree(source_code: String) {
     // Much of the code in this function is adapted from a parse program by Haobo Gu:
     // https://haobogu.github.io/posts/code-intelligence/tree-sitter/
 
@@ -27,7 +27,6 @@ fn build_tree(input_file_contents: String) {
     parser.set_language(tree_sitter_json::language()).unwrap();
 
     // Build a syntax tree based on source code stored in a string.
-    let source_code = input_file_contents;
     let parse_tree: Tree = parser.parse(source_code, None).unwrap();  
 
     // Get the root node of the syntax tree.
@@ -38,12 +37,12 @@ fn build_tree(input_file_contents: String) {
     //let number_node: Node = array_node.named_child(0).unwrap();
 
     // Print the syntax tree as an S-expression.
-    let s_expression = root_node.to_sexp();
+    let s_expression: String = root_node.to_sexp();
     println!();
     println!("Syntax tree: {}", s_expression);
 }
 
 fn main() {
-    let input_file_contents = read_input_file();
+    let input_file_contents: String = read_input_file();
     build_tree(input_file_contents);
 }
